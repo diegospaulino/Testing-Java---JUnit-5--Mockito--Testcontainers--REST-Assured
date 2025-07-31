f9608826af4764504d125192cfe1729190ec42c5
Diego Paulino <diegopaulino023@gmail.com>
2025-07-31 15:21:39 -0300

    Criação de projeto para acompanhar curso de Testes em Java

---
+++ java_testing/src/test/java/com/diegospaulino/studies/java_testing/DemoTest.java
@@ -0,0 +1,11 @@
+package com.diegospaulino.studies.java_testing;
+
+import org.junit.jupiter.api.Test;
+
+public class DemoTest {
+
+    @Test
+    void demoTest() {
+        System.out.println("Demo test is running!");
+    }
+}
---
+++ java_testing/src/test/java/com/diegospaulino/studies/java_testing/JavaTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegospaulino.studies.java_testing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
