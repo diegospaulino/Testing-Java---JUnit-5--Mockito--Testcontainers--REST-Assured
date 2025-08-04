642443776aa418a66e3b721dc04b3ce6414a9828
Diego Paulino <diegopaulino023@gmail.com>
2025-08-04 14:33:45 -0300

    Inclusão de primeira classe de testes com Junit5

---
+++ java_testing/src/main/java/com/diegospaulino/studies/java_testing/junit_five/Calculator.java
@@ -0,0 +1,8 @@
+package com.diegospaulino.studies.java_testing.junit_five;
+
+public class Calculator {
+
+    public int integerDivision(int dividend, int divisor) {
+        return dividend / divisor;
+    }
+}
---
+++ java_testing/src/test/java/com/diegospaulino/studies/java_testing/junit_five/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.diegospaulino.studies.java_testing.junit_five;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    void integerDivision() {
+        Calculator calculator = new Calculator();
+        int result = calculator.integerDivision(4, 2);
+
+        // Assert that the result is as expected
+        assertEquals(2, result, "The integer division of 4 by 2 should be 2"); 
+    }
+}
