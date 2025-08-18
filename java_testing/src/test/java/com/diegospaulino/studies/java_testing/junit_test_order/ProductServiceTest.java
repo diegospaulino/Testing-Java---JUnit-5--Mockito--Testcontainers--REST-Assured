b211650f0fa479729c22f69adbb81d93983a5ecc
Diego Paulino <diegopaulino023@gmail.com>
2025-08-18 19:20:14 -0300

    Ajustes nos métodos de ordenação via properties

--- java_testing/src/test/java/com/diegospaulino/studies/java_testing/junit_test_order/ProductServiceTest.java
+++ java_testing/src/test/java/com/diegospaulino/studies/java_testing/junit_test_order/ProductServiceTest.java
@@ -1,18 +1,18 @@
 package com.diegospaulino.studies.java_testing.junit_test_order;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 @Order(2)
 public class ProductServiceTest {
 
-    // @BeforeAll
-    // static void setup() {
-    //     System.out.println("Segundo...");
-    // }
+    @BeforeAll
+    static void setup() {
+        System.out.println("Segundo...");
+    }
 
     @Test
     void testOrderCreation() {
-        System.out.println("Segundo...");
     }
 }
--- java_testing/src/test/java/com/diegospaulino/studies/java_testing/junit_test_order/UserServiceTest.java
+++ java_testing/src/test/java/com/diegospaulino/studies/java_testing/junit_test_order/UserServiceTest.java
@@ -1,18 +1,18 @@
 package com.diegospaulino.studies.java_testing.junit_test_order;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 
 @Order(1)
 public class UserServiceTest {
 
-    // @BeforeAll
-    // static void setup() {
-    //     System.out.println("Primeiro...");
-    // }
+    @BeforeAll
+    static void setup() {
+        System.out.println("Primeiro...");
+    }
 
     @Test
     void testOrderCreation() {
-        System.out.println("Primeiro...");
     }
 }
